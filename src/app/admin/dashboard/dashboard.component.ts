92805794d57463f0e9bc88b26a8d2a214de9d86b
umutsobe <umutsobee@gmail.com>
2023-08-03 19:28:43 +0300

    lesson #50 created SginalR infrastructure

--- src/app/admin/dashboard/dashboard.component.ts
+++ src/app/admin/dashboard/dashboard.component.ts
@@ -1,7 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { HubUrls } from 'src/app/constants/hub-urls';
+import { ReceiveFunctions } from 'src/app/constants/receive-functions';
+import { SignalRService } from 'src/app/services/common/signal-r.service';
 
 @Component({
   selector: 'app-dashboard',
   template: `<p>DashboardComponent</p>`,
 })
-export class DashboardComponent {}
+export class DashboardComponent implements OnInit {
+  constructor(private signalRService: SignalRService, private toastr: ToastrService) {
+    signalRService.start(HubUrls.ProductHub);
+  }
+  ngOnInit(): void {
+    this.signalRService.on(ReceiveFunctions.ProductAddedMessageReceiveFunction, (message) => {
+      this.toastr.info(message);
+    });
+  }
+}
