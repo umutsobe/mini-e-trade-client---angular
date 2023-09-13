784199e2f3bc9ff64b1adece9dc66ebdcd71f637
umutsobe <umutsobee@gmail.com>
2023-09-13 19:54:12 +0300

    Gitignore updated

--- src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'https://e-trade-apiapi20230820015722.azurewebsites.net/api',
-  signalRUrl: 'https://e-trade-apiapi20230820015722.azurewebsites.net/',
-  googleLoginCredential: '719960856381-r2fd00dus63p054p2cten9o0hl3u4qk5.apps.googleusercontent.com',
-  jwtDomain: 'e-trade-apiapi20230820015722.azurewebsites.net',
-};
--- src/environments/environment.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: false,
-  apiUrl: 'https://localhost:7041/api',
-  signalRUrl: 'https://localhost:7041/',
-  googleLoginCredential: '719960856381-r2fd00dus63p054p2cten9o0hl3u4qk5.apps.googleusercontent.com',
-  jwtDomain: 'localhost:7041',
-};
