30eb16441dfaeeead87000cb2fc36407a8a8ae6d
umutsobe <umutsobee@gmail.com>
2023-09-13 19:44:27 +0300

    environment changes

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: false,
   apiUrl: 'https://localhost:7041/api',
   signalRUrl: 'https://localhost:7041/',
-  googleLoginCredential: '719960856381-8eaoopcgmmkjn2nv91bklf4utlqkc62s.apps.googleusercontent.com',
+  googleLoginCredential: '719960856381-r2fd00dus63p054p2cten9o0hl3u4qk5.apps.googleusercontent.com',
   jwtDomain: 'localhost:7041',
 };
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   apiUrl: 'https://e-trade-apiapi20230820015722.azurewebsites.net/api',
   signalRUrl: 'https://e-trade-apiapi20230820015722.azurewebsites.net/',
-  googleLoginCredential: '719960856381-8eaoopcgmmkjn2nv91bklf4utlqkc62s.apps.googleusercontent.com',
+  googleLoginCredential: '719960856381-r2fd00dus63p054p2cten9o0hl3u4qk5.apps.googleusercontent.com',
   jwtDomain: 'e-trade-apiapi20230820015722.azurewebsites.net',
 };
