44aa9cd361a47b5c7519d6158f1a0599b2bace1b
umutsobe <umutsobee@gmail.com>
2023-09-20 23:18:38 +0300

    heroku deploy settings

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  apiUrl: 'https://localhost:7041/api',
+  signalRUrl: 'https://localhost:7041/',
+  googleLoginCredential: '719960856381-4i73af6alrpf3p3pnlbd9ra0vbbbrfhs.apps.googleusercontent.com',
+  jwtDomain: 'localhost:7041',
+};
