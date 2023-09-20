ae6db64b10647cfc72af1274f4cff5e72ff3f258
umutsobe <umutsobee@gmail.com>
2023-09-20 23:50:46 +0300

    small changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,15 +1,15 @@
-export const environment = {
-  production: true,
-  apiUrl: 'https://localhost:7041/api',
-  signalRUrl: 'https://localhost:7041/',
-  googleLoginCredential: '719960856381-4i73af6alrpf3p3pnlbd9ra0vbbbrfhs.apps.googleusercontent.com',
-  jwtDomain: 'localhost:7041',
-};
-
 // export const environment = {
 //   production: true,
-//   apiUrl: 'https://e-trade-apiapi20230820015722.azurewebsites.net/api',
-//   signalRUrl: 'https://e-trade-apiapi20230820015722.azurewebsites.net/',
+//   apiUrl: 'https://localhost:7041/api',
+//   signalRUrl: 'https://localhost:7041/',
 //   googleLoginCredential: '719960856381-4i73af6alrpf3p3pnlbd9ra0vbbbrfhs.apps.googleusercontent.com',
-//   jwtDomain: 'e-trade-apiapi20230820015722.azurewebsites.net',
+//   jwtDomain: 'localhost:7041',
 // };
+
+export const environment = {
+  production: true,
+  apiUrl: 'https://e-trade-apiapi20230820015722.azurewebsites.net/api',
+  signalRUrl: 'https://e-trade-apiapi20230820015722.azurewebsites.net/',
+  googleLoginCredential: '719960856381-4i73af6alrpf3p3pnlbd9ra0vbbbrfhs.apps.googleusercontent.com',
+  jwtDomain: 'e-trade-apiapi20230820015722.azurewebsites.net',
+};
