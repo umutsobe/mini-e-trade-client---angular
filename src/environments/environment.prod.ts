348493bae425107a621d2845ffe81eb5540b2c66
umutsobe <umutsobee@gmail.com>
2023-09-21 03:10:41 +0300

    prerender routes added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,3 +5,10 @@ export const environment = {
   googleLoginCredential: '719960856381-4i73af6alrpf3p3pnlbd9ra0vbbbrfhs.apps.googleusercontent.com',
   jwtDomain: 'e-commerce-api-umutsobe.azurewebsites.net',
 };
+// export const environment = {
+//   production: false,
+//   apiUrl: 'https://localhost:7041/api',
+//   signalRUrl: 'https://localhost:7041/',
+//   googleLoginCredential: '719960856381-4i73af6alrpf3p3pnlbd9ra0vbbbrfhs.apps.googleusercontent.com',
+//   jwtDomain: 'localhost:7041',
+// };
