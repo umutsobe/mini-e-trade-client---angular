83500bd4ccfecdd4ebd6021f6d75f35eca42bab2
umutsobe <umutsobee@gmail.com>
2023-09-21 03:15:19 +0300

    commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,6 +5,7 @@ export const environment = {
   googleLoginCredential: '719960856381-4i73af6alrpf3p3pnlbd9ra0vbbbrfhs.apps.googleusercontent.com',
   jwtDomain: 'e-commerce-api-umutsobe.azurewebsites.net',
 };
+
 // export const environment = {
 //   production: false,
 //   apiUrl: 'https://localhost:7041/api',
