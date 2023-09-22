3a83a98851db82e406b889b4ce455479184e0ee3
umutsobe <umutsobee@gmail.com>
2023-09-23 00:15:15 +0300

    env changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,15 +1,15 @@
-// export const environment = {
-//   production: true,
-//   apiUrl: 'https://e-commerce-api-umutsobe.azurewebsites.net/api',
-//   signalRUrl: 'https://e-commerce-api-umutsobe.azurewebsites.net/',
-//   googleLoginCredential: '719960856381-4i73af6alrpf3p3pnlbd9ra0vbbbrfhs.apps.googleusercontent.com',
-//   jwtDomain: 'e-commerce-api-umutsobe.azurewebsites.net',
-// };
-
 export const environment = {
-  production: false,
-  apiUrl: 'https://localhost:7041/api',
-  signalRUrl: 'https://localhost:7041/',
+  production: true,
+  apiUrl: 'https://e-commerce-api-umutsobe.azurewebsites.net/api',
+  signalRUrl: 'https://e-commerce-api-umutsobe.azurewebsites.net/',
   googleLoginCredential: '719960856381-4i73af6alrpf3p3pnlbd9ra0vbbbrfhs.apps.googleusercontent.com',
-  jwtDomain: 'localhost:7041',
+  jwtDomain: 'e-commerce-api-umutsobe.azurewebsites.net',
 };
+
+// export const environment = {
+//   production: false,
+//   apiUrl: 'https://localhost:7041/api',
+//   signalRUrl: 'https://localhost:7041/',
+//   googleLoginCredential: '719960856381-4i73af6alrpf3p3pnlbd9ra0vbbbrfhs.apps.googleusercontent.com',
+//   jwtDomain: 'localhost:7041',
+// };
