8e38645b905f3e7b0355da36c5a3589939b376f2
umutsobe <umutsobee@gmail.com>
2023-10-24 00:53:13 +0300

    compose and environment updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,16 +1,17 @@
 export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:7041/api',
-  signalRUrl: 'http://localhost:7041/',
+  production: true,
+  apiUrl: 'http://s_api:7041/api',
+  signalRUrl: 'http://s_api:7041/',
   googleLoginCredential:
     '719960856381-4i73af6alrpf3p3pnlbd9ra0vbbbrfhs.apps.googleusercontent.com',
-  jwtDomain: 'localhost:7041',
+  jwtDomain: 's_api:7041',
 };
 
 // export const environment = {
-//   production: false,
-//   apiUrl: 'https://localhost:7041/api',
-//   signalRUrl: 'https://localhost:7041/',
-//   googleLoginCredential: '719960856381-4i73af6alrpf3p3pnlbd9ra0vbbbrfhs.apps.googleusercontent.com',
-//   jwtDomain: 'localhost:7041',
+//   production: true,
+//   apiUrl: 'http://s_api:7041/api',
+//   signalRUrl: 'http://s_api:7041/',
+//   googleLoginCredential:
+//     '719960856381-4i73af6alrpf3p3pnlbd9ra0vbbbrfhs.apps.googleusercontent.com',
+//   jwtDomain: 's_api:7041',
 // };
