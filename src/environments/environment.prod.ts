a75a3ecb19df5ee595fded5062e4aa36742f2343
umutsobe <umutsobee@gmail.com>
2023-10-23 22:19:55 +0300

    en

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,6 +6,7 @@ export const environment = {
     '719960856381-4i73af6alrpf3p3pnlbd9ra0vbbbrfhs.apps.googleusercontent.com',
   jwtDomain: 'localhost:7041',
 };
+
 // export const environment = {
 //   production: false,
 //   apiUrl: 'https://localhost:7041/api',
