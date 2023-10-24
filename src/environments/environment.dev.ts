d8ef7ce006cf51654a512b1e3c6361612a429d82
umutsobe <umutsobee@gmail.com>
2023-10-24 20:51:30 +0300

    environment update

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -6,11 +6,3 @@ export const environment = {
     '719960856381-4i73af6alrpf3p3pnlbd9ra0vbbbrfhs.apps.googleusercontent.com',
   jwtDomain: 'localhost:7041',
 };
-// export const environment = {
-//   production: false,
-//   apiUrl: 'http://206.81.31.147:777/api',
-//   signalRUrl: 'http://206.81.31.147:777/',
-//   googleLoginCredential:
-//     '719960856381-4i73af6alrpf3p3pnlbd9ra0vbbbrfhs.apps.googleusercontent.com',
-//   jwtDomain: '206.81.31.147:777',
-// };
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,66 @@
+const serverAdress = '206.81.31.147';
+const port = '7041';
+const dockerApiServiceName = 's_api';
+
+const isLocal = true; // localde docker kullanıyorsak
+
+const googleLoginCredential =
+  '719960856381-4i73af6alrpf3p3pnlbd9ra0vbbbrfhs.apps.googleusercontent.com';
 export const environment = {
   production: true,
-  apiUrl: isServer() ? 'http://s_api:7041/api' : 'http://localhost:7041/api',
-  signalRUrl: isServer() ? 'http://s_api:7041/' : 'http://localhost:7041/',
-  googleLoginCredential:
-    '719960856381-4i73af6alrpf3p3pnlbd9ra0vbbbrfhs.apps.googleusercontent.com',
-  jwtDomain: isServer() ? 's_api:7041' : 'localhost:7041',
+  apiUrl: apiUrl(),
+  signalRUrl: signalRUrl(),
+  googleLoginCredential: googleLoginCredential,
+  jwtDomain: jwtDomain(),
 };
 
-function isServer() {
+function isServerSide() {
   return typeof window === 'undefined'; // Sunucu tarafında çalışıyorsa true, istemci tarafında çalışıyorsa false döner
 }
+
+function apiUrl(): string {
+  if (isLocal) {
+    if (isServerSide()) return `http://${dockerApiServiceName}:${port}/api`;
+    else return `http://localhost:${port}/api`;
+  } else {
+    if (isServerSide()) return `http://${dockerApiServiceName}:${port}/api`;
+    else return `http://${serverAdress}:${port}/api`;
+  }
+}
+
+function signalRUrl(): string {
+  if (isLocal) {
+    if (isServerSide()) return `http://${dockerApiServiceName}:${port}/`;
+    else return `http://localhost:${port}/`;
+  } else {
+    if (isServerSide()) return `http://${dockerApiServiceName}:${port}/`;
+    else return `http://${serverAdress}:${port}/`;
+  }
+}
+
+function jwtDomain(): string {
+  if (isLocal) {
+    if (isServerSide()) return `${dockerApiServiceName}:${port}`;
+    else return `localhost:${port}`;
+  } else {
+    if (isServerSide()) return `${dockerApiServiceName}:${port}`;
+    else return `${serverAdress}:${port}`;
+  }
+}
+
+// en son çalışan kodlar
+
+// export const environment = {
+//   production: true,
+//   apiUrl: isServer()
+//     ? "http://s_api:7041/api"
+//     : "http://206.81.31.147:7041/api",
+//   signalRUrl: isServer() ? "http://s_api:7041/" : "http://206.81.31.147:7041/",
+//   googleLoginCredential:
+//     "719960856381-4i73af6alrpf3p3pnlbd9ra0vbbbrfhs.apps.googleusercontent.com",
+//   jwtDomain: isServer() ? "s_api:7041" : "206.81.31.147:7041",
+// };
+
+// function isServer() {
+//   return typeof window === "undefined"; // Sunucu tarafında çalışıyorsa true, istemci tarafında çalışıyorsa false döner
+// }
