8a3341edaca7d2c769eab9466e89f537b21af348
umutsobe <umutsobee@gmail.com>
2023-10-25 20:42:11 +0300

    http interceptor updated

--- src/app/services/common/http-error-handler-interceptor.service.ts
+++ src/app/services/common/http-error-handler-interceptor.service.ts
@@ -1,9 +1,4 @@
-import {
-  HttpEvent,
-  HttpHandler,
-  HttpRequest,
-  HttpStatusCode,
-} from '@angular/common/http';
+import { HttpEvent, HttpHandler, HttpRequest, HttpStatusCode } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 import { Observable, catchError, of } from 'rxjs';
@@ -15,26 +10,21 @@ export class HttpErrorHandlerInterceptorService {
   constructor(private toastr: ToastrService) {}
 
   // app module providersa bak
-  intercept(
-    req: HttpRequest<any>,
-    next: HttpHandler
-  ): Observable<HttpEvent<any>> {
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(req).pipe(
       catchError((error) => {
         switch (error.status) {
           case HttpStatusCode.Unauthorized:
-            this.toastr.warning(
-              'You are not authorized to perform this operation'
-            );
+            this.toastr.warning('You are not authorized to perform this operation');
             break;
 
-          case HttpStatusCode.InternalServerError:
-            this.toastr.warning('Cannot access the server!');
-            break;
+          // case HttpStatusCode.InternalServerError:
+          //   this.toastr.warning('Cannot access the server!');
+          //   break;
 
-          case HttpStatusCode.NotFound:
-            this.toastr.warning('Cannot access the server!');
-            break;
+          // case HttpStatusCode.NotFound:
+          //   this.toastr.warning('Cannot access the server!');
+          //   break;
 
           // default:
           //   this.toastr.warning('An unexpected error occurred!');
